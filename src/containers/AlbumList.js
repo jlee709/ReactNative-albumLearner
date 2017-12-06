e2faa7edff0df92a54ec06c518a071a295b92b57
Justin Lee <leejwg.3@gmail.com>
2017-12-05 20:36:02 -1000

    added axios making api call for list of albums

--- src/containers/AlbumList.js
+++ src/containers/AlbumList.js
@@ -1,9 +1,11 @@
 import React, { Component }  from 'react';
 import { View, Text } from 'react-native';
+import Axios from 'axios';
 
 class AlbumList extends Component {
   componentWillMount(){
-    console.log('called');
+    Axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => console.log(response));
   }
 
 
