334c329021a6439010025639b6abc1fc612410b9
Justin Lee <leejwg.3@gmail.com>
2017-12-05 15:02:42 -1000

    fixed ios build issue, had to blow away ios/build and re-run the xcode build

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from 'react';
+import { Text, AppRegistry } from 'react-native'; 
 
+
+const App = () => 
+  (
+    <Text>Hello</Text>
+  );
+// Handles rendering 
 AppRegistry.registerComponent('albums', () => App);
