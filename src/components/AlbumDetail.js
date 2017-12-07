417553c93c168f806e075b86f34b4a6dae223fd6
Justin Lee <leejwg.3@gmail.com>
2017-12-06 18:23:37 -1000

    main album artwork renders

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,29 +4,60 @@ import Card from './Card';
 import CardItem from './CardItem';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    thumbnailStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    headerContentStyle,
+    imageStyle
+  } = styles;
 
   return (
     <Card>
       <CardItem>
-
-      <View>
+      <View style={thumbnailContainerStyle}>
         <Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
       </View>
-
         <View style={styles.headerContentStyle}>
-         <Text>{ title }</Text>
+         <Text style={headerTextStyle}>{ title }</Text>
          <Text>{ artist }</Text>
         </View>
+      </CardItem>
+      <CardItem>
+        <Image
+        style={imageStyle} 
+        source={{ uri: image }} />
       </CardItem>
     </Card>
   );
 }; 
  
  const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+
+  headerTextStyle:{
+    fontSize: 18
+  },
+
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
  }
 
