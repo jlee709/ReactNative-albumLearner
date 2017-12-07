4b7fb1f5915643cd60e01f3d44757232275d1a22
Justin Lee <leejwg.3@gmail.com>
2017-12-06 18:28:46 -1000

    can now scroll through the album list;

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import Header from './src/components/Header';
 import AlbumList from './src/containers/AlbumList';
 
 const App = () => (
-  <View>
+  <View style={{flex: 1 }}>
     <Header headerText={'Albums'}/>
     <AlbumList />
   </View>
