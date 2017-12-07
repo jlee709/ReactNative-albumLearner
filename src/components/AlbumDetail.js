a15f6db69cc7698ec65258efcfa555284b8c9f9c
Justin Lee <leejwg.3@gmail.com>
2017-12-06 17:57:56 -1000

    can render image thumbnail from api call

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardItem from './CardItem';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album;
+
   return (
     <Card>
       <CardItem>
-       <Text>{props.album.title} </Text>
+
+      <View>
+        <Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
+      </View>
+
+        <View style={styles.headerContentStyle}>
+         <Text>{ title }</Text>
+         <Text>{ artist }</Text>
+        </View>
       </CardItem>
     </Card>
   );
 }; 
+ 
+ const styles = {
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  }
+ }
 
 export default AlbumDetail;
