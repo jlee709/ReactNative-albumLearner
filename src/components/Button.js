fb954daa33954a2bd21870af7e5558a21f0ac4c0
Justin Lee <leejwg.3@gmail.com>
2017-12-08 15:20:41 -1000

    buttons working

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,37 @@ import React from 'react';
 import  { Text, TouchableOpacity, } from 'react-native';
 
 const Button = () => {
+  const { buttonStyle, textStyle } = styles;
+
   return(
-    <TouchableOpacity>
-      <Text>Purchase</Text>
+    <TouchableOpacity style={buttonStyle}>
+      <Text style={textStyle}>
+        Purchase Here!
+      </Text>
     </TouchableOpacity>
   );
 } 
+
+const styles = {
+  textStyle: {
+    alignSelf: 'center',
+    color: '#0000FF',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+
+  buttonStyle: {
+    felx: 1,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#0000FF',
+    margineLeft: 5,
+    margineRight: 5
+
+  }
+}
 
 export default Button;
