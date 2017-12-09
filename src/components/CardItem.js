7ec15089f6ec56b42d3835fcd2f8b43d9a5484ab
Justin Lee <leejwg.3@gmail.com>
2017-12-08 17:08:53 -1000

    fixed render bugs cleaned up

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -13,7 +13,7 @@ const styles = {
   containerStyle: {
     borderBottomWidth: 1,
     padding: 7,
-    backGroundColor: '#ddd;',
+    backgroundColor: '#fff',
     justifyContent: 'flex-start',
     flexDirection: 'row',
     borderColor: '#ddd',
