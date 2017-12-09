0494d65d9a8dfe7ef8e8c932c4979d397d851a99
Justin Lee <leejwg.3@gmail.com>
2017-12-08 17:31:59 -1000

    refactoring

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -34,7 +34,9 @@ const AlbumDetail = ({ album }) => {
       </CardItem>
 
       <CardItem>
-        <Button onPress={ () => Linking.openURL(url)}/>
+        <Button onPress={ () => Linking.openURL(url)}>
+          Purchase Here
+        </Button>
       </CardItem>
     </Card>
   );
